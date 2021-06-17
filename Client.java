cb82ae516e97d24a2cb08e6e2cccb667e57e035c
白哥 <80096029+halo-luo@users.noreply.github.com>
2021-06-17 21:07:29 +0800

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,30 @@
+package Mediator;
+
+public class Client {
+    public static void main(String[] args) {
+        AbstractChatroom happyChat = new ChatGroup();
+        Member member1,member2,member3,member4,member5;
+        member1 = new DiamondMember("张三");
+        member2 = new DiamondMember("李四");
+        member3 = new CommonMember("王五");
+        member4 = new CommonMember("小芳");
+        member5 = new CommonMember("小红");
+
+        happyChat.register(member1);
+        happyChat.register(member2);
+        happyChat.register(member3);
+        happyChat.register(member4);
+        happyChat.register(member5);
+
+        member1.sendText("李四","李四，你好");
+        member2.sendText("张三","张三，你好");
+        member1.sendText("李四","天气真好,可以打鱼");
+        member2.sendImage("张三","一个很大很大大大大大大大的大太阳");
+        member2.sendImage("张三","太阳");
+        member3.sendText("小芳","还有问题吗");
+        member3.sendText("小红","还有问题吗");
+        member4.sendText("王五","没有了");
+        member5.sendText("王五","我也没有了");
+        member5.sendImage("王五","谢谢");
+    }
+}
---
+++ CommonMember.java
@@ -0,0 +1,14 @@
+package Mediator;
+
+public class CommonMember extends Member{
+    public CommonMember(String name){
+        super(name);
+    }
+    public void sendText(String to,String message){
+        System.out.println("普通会员发消息：");
+        chatroom.sendText(name,to,message);
+    }
+    public void sendImage(String to,String image){
+        System.out.println("普通会员不能发送图片！");
+    }
+}
